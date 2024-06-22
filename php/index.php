990afb8f53779aa3255613f1e21dd7caa97bcd65
Matthias von Bargen <matt@mavon.ie>
2024-06-22 20:02:14 +0200

    cache handler

--- php/index.php
+++ php/index.php
@@ -1,4 +1,17 @@
 <?php
 
+$url = $_SERVER['REQUEST_URI'];
+$path = str_replace('/php/', '', $url);
 
-echo "LOL";
+$key = str_replace(
+    '.html',
+    '',
+    $path
+);
+
+$content = file_get_contents('/static/' . $path);
+
+header('Surrogate-Key: php-' . $key . ' ' . $key);
+header('Cache-Control: max-age=300, public');
+
+echo $content;
